28733df851485f39c9d156c72f05a741c438fca0
Andrew Sellers <andrew@andrewcsellers.com>
2015-09-05 03:24:11 -0500

    Fix room sprite test

--- game/vault_test.go
+++ game/vault_test.go
@@ -6,7 +6,7 @@ func TestRoomSprite(t *testing.T) {
 	v := NewVault()
 	r := v.PlaceRoom(9, 0, 4)
 	sp := v.Rooms[r].Sprite()
-	if sp != "food1/level_1/size_3/sprite.png" {
-		t.Fatal("Expected: food1u1x3, Found:", sp)
+	if sp != "rooms/food1/level_1/size_3/sprite.png" {
+		t.Fatal("Expected: 'rooms/food1/level_1/size_3/sprite.png', Found:", sp)
 	}
 }
