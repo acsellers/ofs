38ddb2fa2f1ff9fca0cdd5c86050bb323580379d
Andrew Sellers <andrew@andrewcsellers.com>
2015-09-05 03:23:15 -0500

    Fix misaligned rooms in tests

--- game/vault_search_test.go
+++ game/vault_search_test.go
@@ -4,9 +4,9 @@ import "testing"
 
 func TestVaultRouting(t *testing.T) {
 	v := NewVault()
-	v.PlaceRoom(10, 0, 1)
-	v.PlaceRoom(10, 1, 1)
-	to := v.PlaceRoom(11, 1, 2)
+	v.PlaceRoom(9, 0, 1)
+	v.PlaceRoom(9, 1, 1)
+	to := v.PlaceRoom(10, 1, 2)
 	path := v.Route(1, to)
 	if len(path) == 0 {
 		t.Fatal("No Path?")
--- game/vault_test.go
+++ game/vault_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestRoomSprite(t *testing.T) {
 	v := NewVault()
-	r := v.PlaceRoom(10, 0, 4)
+	r := v.PlaceRoom(9, 0, 4)
 	sp := v.Rooms[r].Sprite()
 	if sp != "food1/level_1/size_3/sprite.png" {
 		t.Fatal("Expected: food1u1x3, Found:", sp)
